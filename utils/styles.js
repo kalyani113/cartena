c1dca1c996cf8902defeaed4674dc32d0b89d2d7
kalyani-WAL1 <kalyani.a@westagilelabs.com>
2022-01-05 08:30:57 +0530

    Added layout and Material Ui styles

---
+++ utils/styles.js
@@ -0,0 +1,18 @@
+import {makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  appBar: {
+    backgroundColor: '#232f3e',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10
+    }
+  },
+  main: {
+    minHeight: '80vh'
+  },
+  footer: {
+    textAlign: 'center'
+  }
+});
+export default useStyles;
